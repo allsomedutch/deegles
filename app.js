7c4767629af32b845a6e4644f6d209788c8794b0
Oladotun Sobande <oladutch@gmail.com>
2016-07-18 12:42:19 +0100

    Update nodejs modules - 2

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/**
+* APP:       DEEGLES
+* COMPANY:   Allsome Corp.
+*
+* MODULE:    MS Core Module
+*
+* DEVELOPER: Oladotun Sobande
+* DATE:      18th July 2016
+*/
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
