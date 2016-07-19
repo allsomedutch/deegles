9cad20da6b4a9f88691e967280e4dd8d65f1567e
Oladotun Sobande <oladutch@gmail.com>
2016-07-19 17:34:41 +0100

    client-detect dev

--- client-detect.js
+++ client-detect.js
@@ -7,3 +7,46 @@
  * DEVELOPER: Oladotun Sobande
  * DATE:      18th July 2016
  */
+
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var logger = require('morgan');
+var MobileDetect = require('mobile-detect');
+
+var app = express();
+
+var lf = fs.createWriteStream(path.join('logs/logger.log'), { flag : 'a' });
+app.use(logger('common', {stream : lf}));
+
+function UserClient(req){
+    var self = this;
+    this.status = false;
+    this.isMobile(req);
+}
+
+UserClient.prototype.isMobile = function(req){
+    var self = this;
+    self.md = new MobileDetect(req.headers['user-agent']);
+    self.os = self.md.os();
+
+    switch(self.os) {
+        case "AndroidOS":
+            this.status = true;
+            break;
+        case "iOS":
+            this.status = true;
+            break;
+        case "WindowsPhoneOS":
+            this.status = true;
+            break;
+        case null:
+            this.status = false;
+            break;
+    }
+    return this.status;
+};
+
+exports.checkDevice = function(req){
+    var isMobile = new UserClient(req);
+};
